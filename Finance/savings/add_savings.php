3bc0990e94ad46d7dc03c3d6a26f338277931d56
Caleb Yarborough <121351919+Caleb-Yarborough@users.noreply.github.com>
2024-12-04 11:27:17 -0500

    Update add_savings.php
    Fixed formatting issue

--- Finance/savings/add_savings.php
+++ Finance/savings/add_savings.php
@@ -1,43 +1,43 @@
 <?php
-	ini_set('display_errors', 1);
+ini_set('display_errors', 1);
 //Name: Caleb Yarborough
 /*This code assumes user input is valid and correct only for demo purposes - it does NOT validate form data.*/
-	if(isset($_POST['submit'])) { //Form was submitted
+if(isset($_POST['submit'])) { //Form was submitted
 		
-		try{
-			require_once('../../../pdo_connect.php'); 
+	try{
+		require_once('../../../pdo_connect.php'); 
 
-			$sql = 'INSERT INTO Savings (SavingsID, UserID, GoalName, CurrentAmount, GoalAmount, InterestRate, TargetDate) 
-			VALUES (:SavingsID, :UserID, :GoalName, :CurrentAmount, :GoalAmount, :InterestRate, :TargetDate)';
+		$sql = 'INSERT INTO Savings (SavingsID, UserID, GoalName, CurrentAmount, GoalAmount, InterestRate, TargetDate) 
+		VALUES (:SavingsID, :UserID, :GoalName, :CurrentAmount, :GoalAmount, :InterestRate, :TargetDate)';
 			
-			$stmt = $dbc->prepare($sql);
-			$stmt->bindParam(':SavingsID', $_POST['SavingsID'], PDO::PARAM_INT);
-			$stmt->bindParam(':UserID', $_POST['UserID'], PDO::PARAM_INT);
-			$stmt->bindParam(':GoalName', $_POST['GoalName'], PDO::PARAM_STR);
-			$stmt->bindParam(':CurrentAmount', $_POST['CurrentAmount'], PDO::PARAM_INT);
-            $stmt->bindParam(':GoalAmount', $_POST['GoalAmount'], PDO::PARAM_INT);
-			$stmt->bindParam(':InterestRate', $_POST['InterestRate'], PDO::PARAM_INT);
-			$stmt->bindParam(':TargetDate', $_POST['TargetDate'], PDO::PARAM_STR);
+		$stmt = $dbc->prepare($sql);
+		$stmt->bindParam(':SavingsID', $_POST['SavingsID'], PDO::PARAM_INT);
+		$stmt->bindParam(':UserID', $_POST['UserID'], PDO::PARAM_INT);
+		$stmt->bindParam(':GoalName', $_POST['GoalName'], PDO::PARAM_STR);
+		$stmt->bindParam(':CurrentAmount', $_POST['CurrentAmount'], PDO::PARAM_INT);
+                $stmt->bindParam(':GoalAmount', $_POST['GoalAmount'], PDO::PARAM_INT);
+		$stmt->bindParam(':InterestRate', $_POST['InterestRate'], PDO::PARAM_INT);
+		$stmt->bindParam(':TargetDate', $_POST['TargetDate'], PDO::PARAM_STR);
 
 
-			$stmt->execute();	
+		$stmt->execute();	
             
-		} catch (PDOException $e){
-			echo $e->getMessage();
-		}	
-		$affected = $stmt->RowCount();
-		if ($affected == 0){
-			echo "We could not insert into Savings.";
-			exit;
-		}	
-		else {
-			$result = $stmt->fetchAll();
-		}
-	} //end isset
-	else {
-		echo "<h2>You have reached this page in error</h2>";
+	} catch (PDOException $e){
+		echo $e->getMessage();
+	}	
+	$affected = $stmt->RowCount();
+	if ($affected == 0){
+		echo "We could not insert into Savings.";
 		exit;
+	}	
+	else {
+		$result = $stmt->fetchAll();
 	}
+} //end isset
+else {
+	echo "<h2>You have reached this page in error</h2>";
+	exit;
+}
 	
 ?>
 <!DOCTYPE html>
